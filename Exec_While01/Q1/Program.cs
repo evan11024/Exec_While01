2dc44283c2f7a0d60244a5ebe86a9f90eb2014cd
evan11024 <115928567+evan11024@users.noreply.github.com>
2022-10-23 15:04:41 +0800

    Add files via upload

---
+++ Exec_While01/Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n = 0;
+            while (sum < 105)
+            {
+                sum+=n;
+                n++;
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ Exec_While01/Q2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 300; i++)
+            {
+                while (300 % i == 0)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+        }
+    }
+}
